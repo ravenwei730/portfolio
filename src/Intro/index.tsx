f09e6792f4e1d3b023aac571a8ba308eefb00109
Han Chen <hanmikechen@gmail.com>
2023-08-16 12:45:53 -0400

    update intro

--- src/Intro/index.tsx
+++ src/Intro/index.tsx
@@ -3,12 +3,11 @@ import React from "react";
 const Intro = () => {
 
     const introduction1 = 
-    "I'm a Business Analayst at Stellar and a former Data Instructor and Account Executive. My software development journey began when I took a decisive step and joiLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 
 
     const introduction2 = 
     "When I'm not coding, I enjoy exploring hobby 1, hobby 2, hobby 3, more..."
-    
 
     return ( 
         <div className="self-center mt-24 w-1/2 text-xl text-center font-Mont sm:w-3/4 sm:mt-10 sm:text-base">
