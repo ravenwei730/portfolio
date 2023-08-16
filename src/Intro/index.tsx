e1c6334da35fdeecd2c1db0862157f31a1160b93
Han Chen <hanmikechen@gmail.com>
2023-08-16 12:44:43 -0400

    update intro

--- src/Intro/index.tsx
+++ src/Intro/index.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const Intro = () => {
 
     const introduction1 = 
-    "I'm a Full Stack Software Developer at Stellar and a former Data Instructor and Account Executive. My software development journey began when I took a decisive step and joined App Academy in the summer of 2022. Since then, I've thoroughly enjoyed learning new technologies and the feeling of finally figuring out the solution to a problem."
+    "I'm a Business Analayst at Stellar and a former Data Instructor and Account Executive. My software development journey began when I took a decisive step and joiLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
 
 
     const introduction2 = 
-    "When I'm not coding, I enjoy exploring national parks, cycling, playing sports, and catching up on movies."
+    "When I'm not coding, I enjoy exploring hobby 1, hobby 2, hobby 3, more..."
     
 
     return ( 
