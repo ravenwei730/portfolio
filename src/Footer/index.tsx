6aa79eda05908e789e01cc1c753f1e353095703e
Han Chen <hanmikechen@gmail.com>
2024-07-19 09:56:00 -0400

    update styling

--- src/Footer/index.tsx
+++ src/Footer/index.tsx
@@ -3,8 +3,7 @@ import "./Footer.css";
 
 const Footer = () => {
     return ( 
-        <div className="footer font-Mont mt-60 self-center w-5/6 h-24 border-blue-500 text-xs text-center py-2 justify-between flex sm:mt-24 sm:h-14 sm:text-xxs sm:w-11/12">
-            <span className="sm:text-left">Designed & Built by: Han Chen 2023</span>
+        <div className="footer font-Mont mt-60 self-center w-5/6 h-24 border-blue-500 text-xs text-center py-2 justify-center flex sm:mt-24 sm:h-14 sm:text-xxs sm:w-11/12">
             <span>All right reserved.</span>
         </div>
      );
